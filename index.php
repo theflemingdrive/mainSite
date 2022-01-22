04634b24383dccd78637977ebb74a46a0a1d9e10
Sid <67176073+theflemingdrive@users.noreply.github.com>
2022-01-21 22:17:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="sid-plesk">
+<head>
+    <title>The Fleming Drive</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Stylesheets -->
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Bootstrap 3 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Scripts -->
+
+</head>
+<body>
+
+  <header>
+    <span id="siteLogo"><img src="/resource/img/logoSmall.png" alt="Fleming Drive Logo" width="100%" height="auto"></span>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">TheFlemingDrive</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li class="active"><a href="contact.php">Contact Me</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Work Data
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="workData/rod_calc.php">Rod Calculator</a></li>
+                  <li><a href="workData/die_speeds.php">Die Speeds</a></li>
+                </ul>
+            </li>
+            <li><a href="#" id="workLoginButton"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+	</header>
+
+	<main>
+	</main>
+
+	<footer>
+	</footer>
+
+</body>
+</html>
