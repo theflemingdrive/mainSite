107d43eb7b69dd94f4b05f2aadb547ec986baffd
Sid <admin@theflemingdrive.com>
2022-01-22 02:28:57 -0600

    Update sidebarNav.js

--- resource/js/sidebarNav.js
+++ resource/js/sidebarNav.js
@@ -2,7 +2,7 @@ var app = angular.module("sideNav", ["ngRoute"]);
 app.config(function($routeProvider) {
   $routeProvider
   .when("/", {
-    templateUrl : "/../sideNav/index.htm"
+    templateUrl : "//sideNav/index.htm"
   })
   .when("/vectorAdd", {
     templateUrl : "//sideNav/calc/vectorAdd.php"
