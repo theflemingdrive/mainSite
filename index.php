48f5c1d5b352db9b72c15e5f0edd80e00ce6b534
Sid <admin@theflemingdrive.com>
2022-01-22 02:17:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">TheFlemingDrive</a>
+          <a class="navbar-brand" href="#/!">TheFlemingDrive</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
