a65843d3fcff748186df18f1997268da5ac5967a
Sid <admin@theflemingdrive.com>
2022-01-25 23:41:24 -0600

    Update mathFunctions.php

--- resource/include/mathFunctions.php
+++ resource/include/mathFunctions.php
@@ -2,10 +2,14 @@
 
 function absVal($absVIn) {
   if ($absVIn > 0) {
-    echo '<p>' . $absVIn . '</p>';
+    echo $absVIn;
   } else {
-    echo '<p>' . $absVIn - $absVIn . '</p>';
+    echo $absVIn - $absVIn;
   }
+}
+
+function returnUnitVec($vecMag, $vecAMag, $vecBMag) {
+  echo sqrt(pow($vecMag, 2) - pow($vecAMag, 2) - pow($vecBMag, 2);
 }
 
 ?>
