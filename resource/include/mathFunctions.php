76eb9d2508c1494fcfdc262b56e9f7516e0f7736
Sid <admin@theflemingdrive.com>
2022-01-25 22:59:48 -0600

    Update mathFunctions.php

--- resource/include/mathFunctions.php
+++ resource/include/mathFunctions.php
@@ -1,10 +1,10 @@
 <?php
 
-function absVal(absVIn) {
-  if (absVIn > 0) {
-    return absVIn;
+function absVal($absVIn) {
+  if ($absVIn > 0) {
+    return $absVIn;
   } else {
-    return absVIn - absVIn - absVIn;
+    return $absVIn - $absVIn - $absVIn;
   }
 }
 
