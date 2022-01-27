210f98d265ced753aba4e5ae352e035a81176b19
Sid <admin@theflemingdrive.com>
2022-01-26 21:04:12 -0600

    Update oeeCalc.php

--- oeeCalc.php
+++ oeeCalc.php
@@ -3,16 +3,22 @@
 <body>
   <form>
     <h1>OEE Calculator</h1>
+    <hr>
     <label for="actRuntime">Actual Runtime (minutes): </label>
-    <input type="number" name="actRuntime" id="actRuntime"><br>
+    <input type="number" name="actRuntime" id="actRuntime">
+    <br><br>
     <label for="totalRuntime">Total Runtime (minutes): </label>
-    <input type="number" name="totalRuntime" id="totalRuntime"><br>
+    <input type="number" name="totalRuntime" id="totalRuntime">
+    <br><br>
     <label for="cycleTime">PPM: </label>
-    <input type="number" name="cycleTime" id="cycleTime"><br>
+    <input type="number" name="cycleTime" id="cycleTime">
+    <br><br>
     <label for="actParts">Total Parts Ran: </label>
-    <input type="number" name="actParts" id="actParts"><br>
+    <input type="number" name="actParts" id="actParts">
+    <br><br>
     <label for="badParts">Scrap Count: </label>
-    <input type="number" name="badParts" id="badParts"><br><br>
+    <input type="number" name="badParts" id="badParts">
+    <br><br>
     <input type="submit" placeholder="Submit">
   </form>
 </body>
