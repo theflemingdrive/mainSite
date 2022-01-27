2643b3dd985c79bad35ca0a5e20ada1e8711dc3a
Sid <admin@theflemingdrive.com>
2022-01-26 20:59:45 -0600

    Create oeeCalc.php

---
+++ oeeCalc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <form>
+    <h1>OEE Calculator</h1>
+    <label for="actRuntime">Actual Runtime (minutes): </label>
+    <input type="number" name="actRuntime" id="actRuntime"><br>
+    <label for="totalRuntime">Total Runtime (minutes): </label>
+    <input type="number" name="totalRuntime" id="totalRuntime"><br>
+    <label for="cycleTime">PPM: </label>
+    <input type="number" name="cycleTime" id="cycleTime"><br>
+    <label for="actParts">Total Parts Ran: </label>
+    <input type="number" name="actParts" id="actParts"><br>
+    <label for="badParts">Scrap Count: </label>
+    <input type="number" name="badParts" id="badParts"><br><br>
+    <input type="submit" placeholder="Submit">
+  </form>
+</body>
+</html>
