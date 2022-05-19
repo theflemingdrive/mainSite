dba3eb886c3e4f81f29826d187a63e0ca6c6c8e3
Sid <admin@theflemingdrive.com>
2022-05-19 08:42:24 -0500

    Update aladdin.php

--- aladdin.php
+++ aladdin.php
@@ -15,7 +15,7 @@
     }
 
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 850px}
+    .row.content {height: 825px}
 
     /* Set gray background color and 100% height */
     .sidenav {
