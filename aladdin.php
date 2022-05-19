5f1ccb2a896c3168c3558794a07ad859d8e97524
Sid <admin@theflemingdrive.com>
2022-05-19 08:59:40 -0500

    Update aladdin.php

--- aladdin.php
+++ aladdin.php
@@ -54,7 +54,7 @@
       <a class="navbar-brand" href="#">Logo</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <img src="/resource/img/alBanner.jpg" height="15%">
+      <h1>Aladdin Maintenace</h1>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
       </ul>
@@ -65,9 +65,6 @@
 <div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
     </div>
     <div class="col-sm-8 text-left">
       <h1>Welcome</h1>
@@ -77,12 +74,7 @@
       <p>Lorem ipsum...</p>
     </div>
     <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>ADS</p>
-      </div>
-      <div class="well">
-        <p>ADS</p>
-      </div>
+      
     </div>
   </div>
 </div>
